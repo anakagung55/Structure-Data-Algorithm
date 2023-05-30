19d535b0e1b0fd9a1c3c095035d25e13bd15c5c3
AgungBagus_ILKOM-IKI <127914636+anakagung55@users.noreply.github.com>
2023-05-30 23:36:00 +0800

    Add files via upload

---
+++ queue/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "queue.h"
+using namespace std;
+
+int main(int argc, char** argv) {
+	Queue queue1;
+	
+	queue1.enqueue(10);
+	queue1.enqueue(20);
+	queue1.enqueue(30);
+	queue1.enqueue(40);
+	queue1.dequeue();
+	
+	cout <<"rear: " <<queue1.rear->value<<endl;
+	cout <<"front: " <<queue1.front->value<<endl;
+	
+	queue1.printAll();
+	return 0;
+}
+
---
+++ queue/queue.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "queue.h"
+using namespace std;
+
+void Queue::enqueue(int input){
+	Node* temp = new Node;
+        temp->value = input;
+        temp->next = NULL;
+        if (front == NULL) {
+            front = temp;
+        } else {
+            rear->next = temp;
+        }
+        rear = temp;
+}
+void Queue::dequeue(){
+	if (front == NULL) {
+            cout << "Queue is empty." << endl;
+            return;
+        }
+        Node* temp = front;
+        front = front->next;
+        delete temp;
+}
+void Queue::printAll(){
+	Node *tmp = front;
+	while(tmp!=NULL){
+		cout<<tmp->value<<"->";
+		tmp = tmp->next;
+	}
+	cout<<endl;
+}
+
